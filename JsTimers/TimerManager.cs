57da37b97155c9a7d7da1058dbfb837a6ac8befa
2CHEVSKII <prsroman3@gmail.com>
2021-09-23 17:26:31 +0300

    Various public API methods and helpers

--- JsTimers/TimerManager.cs
+++ JsTimers/TimerManager.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -65,6 +67,28 @@ namespace JsTimers
             return timeout;
         }
 
+        public static Timeout SetTimeout(Action callback, float delay)
+        {
+            return SetTimeout(callback, ConvertToMilliseconds(delay));
+        }
+
+        public static Timeout SetInterval(Action callback, int interval)
+        {
+            if (callback is null)
+            {
+                throw new ArgumentNullException(nameof(callback), "Callback must be a function. Received null");
+            }
+
+            var timeout = new Timeout(callback, interval, true);
+            ActiveTimeouts.Add(timeout);
+            return timeout;
+        }
+
+        public static Timeout SetInterval(Action callback, float interval)
+        {
+            return SetInterval(callback, ConvertToMilliseconds(interval));
+        }
+
         public static Immediate SetImmediate(Action callback)
         {
             if (callback is null)
@@ -80,16 +104,18 @@ namespace JsTimers
             return immediate;
         }
 
-        public static Timeout SetInterval(Action callback, int interval)
+        public static Immediate SetImmediate(object obj, string methodName, params object[] args)
         {
-            if (callback is null)
-            {
-                throw new ArgumentNullException(nameof(callback), "Callback must be a function. Received null");
-            }
+            var callback = GetCallbackFromInstanceMethod(obj, methodName, args);
 
-            var timeout = new Timeout(callback, interval, true);
-            ActiveTimeouts.Add(timeout);
-            return timeout;
+            return SetImmediate(callback);
+        }
+
+        public static Immediate SetImmediate(Type type, string methodName, params object[] args)
+        {
+            var callback = GetCallbackFromStaticMethod(type, methodName, args);
+
+            return SetImmediate(callback);
         }
 
         public static void ClearInterval(Timeout interval)
@@ -116,16 +142,6 @@ namespace JsTimers
         }
 
         #endregion
-
-        static void ClearTimer(Timeout timer)
-        {
-            if (timer is null || timer.Destroyed)
-            {
-                return;
-            }
-
-            timer.DestroyNow();
-        }
 
         internal static void RefMe(Timer timer)
         {
@@ -169,6 +185,104 @@ namespace JsTimers
             ActiveTimeouts.Add(timeout);
         }
 
+        static void ClearTimer(Timeout timer)
+        {
+            if (timer is null || timer.Destroyed)
+            {
+                return;
+            }
+
+            timer.DestroyNow();
+        }
+
+        static Action GetCallbackFromStaticMethod(Type type, string methodName, object[] args)
+        {
+            if (type is null)
+            {
+                throw new ArgumentNullException(nameof(type), "Target object cannot be null");
+            }
+
+            if (string.IsNullOrWhiteSpace(methodName))
+            {
+                throw new ArgumentException($"'{methodName ?? "null"}' is not a valid method identifier");
+            }
+
+            var methodInfo = type.GetMethod(
+                methodName,
+                BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic
+            );
+
+            if (methodInfo is null)
+            {
+                throw new InvalidOperationException($"Method '{methodName}' was not found in type {type.Name}");
+            }
+
+            return BuildCallback(methodInfo, args);
+        }
+
+        static Action GetCallbackFromInstanceMethod(object obj, string methodName, object[] args)
+        {
+            if (obj is null)
+            {
+                throw new ArgumentNullException(nameof(obj), "Target object cannot be null");
+            }
+
+            if (string.IsNullOrWhiteSpace(methodName))
+            {
+                throw new ArgumentException($"'{methodName ?? "null"}' is not a valid method identifier");
+            }
+
+            var type = obj.GetType();
+
+            var methodInfo = type.GetMethod(
+                methodName,
+                BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic
+            );
+
+            if (methodInfo is null)
+            {
+                throw new InvalidOperationException($"Method '{methodName}' was not found in type {type.Name}");
+            }
+
+            var aargs = new object[args.Length + 1];
+            aargs[0] = obj;
+            args.CopyTo(aargs, 1);
+
+            return BuildCallback(methodInfo, aargs);
+        }
+
+        static Action BuildCallback(MethodInfo methodInfo, params object[] args)
+        {
+            if (methodInfo.IsStatic)
+            {
+                return () =>
+                {
+                    methodInfo.Invoke(null, args);
+                };
+            }
+
+            if (args.Length == 1)
+            {
+                return () =>
+                {
+                    methodInfo.Invoke(args[0], Array.Empty<object>());
+                };
+            }
+
+            var obj = args[0];
+            var remainingArgs = new object[args.Length - 1];
+
+            for (var i = 1; i < args.Length; i++)
+            {
+                remainingArgs[i - 1] = args[i];
+            }
+
+            return () =>
+            {
+                methodInfo.Invoke(obj, remainingArgs);
+            };
+        }
+
         static void ProcessTick()
         {
             lock (NextTickQueue)
@@ -199,6 +313,11 @@ namespace JsTimers
                     timeout.SafeExecute();
                 }
             }
+        }
+
+        static int ConvertToMilliseconds(float seconds)
+        {
+            return (int)(seconds * 1000);
         }
     }
 }
