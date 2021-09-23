809435b9c0570f9268ae99caaf8d4234a275ee97
2CHEVSKII <prsroman3@gmail.com>
2021-09-23 18:59:46 +0300

    Add MSTest proj, delete placeholders

---
+++ JsTimers.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace JsTimers.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
