a5ee4c5554419599c9926a257ab771c704adba43
2CHEVSKII <prsroman3@gmail.com>
2021-09-23 18:44:31 +0300

    Placeholder main

---
+++ JsTimers.Tests.ConsoleApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using JsTimers;
+
+namespace JsTimers.Tests.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Started JsTimers.ConsoleApp");
+
+            // Placeholder
+
+            Console.WriteLine("Finished the Main method");
+        }
+    }
+}
