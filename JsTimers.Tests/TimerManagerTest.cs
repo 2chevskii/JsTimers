b8e140c6683070b76ead6b1204548ee28becc111
2CHEVSKII <prsroman3@gmail.com>
2022-03-23 20:48:53 +0300

    more tests for timerManager

--- JsTimers.Tests/TimerManagerTest.cs
+++ JsTimers.Tests/TimerManagerTest.cs
@@ -43,6 +43,48 @@ namespace JsTimers.Tests
         }
 
         [TestMethod]
+        public void SetRefEnableDisableTestTimeout()
+        {
+            Timeout timeout = SetTimeout(Noop, ZERO);
+            
+            Assert.IsTrue(timeout.HasRef());
+
+            timeout.UnRef();
+            Assert.IsFalse(timeout.HasRef());
+
+            timeout.Ref();
+            Assert.IsTrue(timeout.HasRef());
+        }
+
+        [TestMethod]
+        public void SetRefEnableDisableTestInterval()
+        {
+            Timeout interval = SetTimeout(Noop, ZERO);
+
+            Assert.IsTrue(interval.HasRef());
+
+            interval.UnRef();
+            Assert.IsFalse(interval.HasRef());
+
+            interval.Ref();
+            Assert.IsTrue(interval.HasRef());
+        }
+
+        [TestMethod]
+        public void SetRefEnableDisableTestImmediate()
+        {
+            Immediate immediate = SetImmediate(Noop);
+
+            Assert.IsTrue(immediate.HasRef());
+
+            immediate.UnRef();
+            Assert.IsFalse(immediate.HasRef());
+
+            immediate.Ref();
+            Assert.IsTrue(immediate.HasRef());
+        }
+
+        [TestMethod]
         public void SetTimeoutTest()
         {
             var testStartTime = DateTime.Now;
