d8c79beac64ab808b95cf748ca2c1a2845a17a17
2CHEVSKII <prsroman3@gmail.com>
2022-03-25 20:24:31 +0300

    try to deploy on appveyor -> gh pages

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vitepress';
 
 export default defineConfig({
+  base: '/jstimers/',
   lang: 'en-US',
   title: 'JsTimers',
   description: 'Simple timer management for .NET',
