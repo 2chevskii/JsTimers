a065e3649e9997f249888b41e43196ee3e7273a1
2CHEVSKII <prsroman3@gmail.com>
2022-03-26 03:31:42 +0300

    Update VitePress config
    - Fix base

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vitepress';
 
 export default defineConfig({
-  base: '/jstimers/',
+  base: '/JsTimers',
   lang: 'en-US',
   title: 'JsTimers',
   description: 'Simple timer management for .NET',
